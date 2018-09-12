85e22e6e5465dca87ed0f642f30fed757b0a2f25
yljasmine <yljasmine96@gmail.com>
2018-09-12 09:37:44 +0900

    Initial Commit

---
+++ Hello/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello
+//
+//  Created by lujia_yang on 12/9/18.
+//  Copyright © 2018 lujia_yang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
